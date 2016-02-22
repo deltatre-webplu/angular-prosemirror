03cab763d5c3e3917538c2095e81ed79f626aaf7
Davide Icardi <davide.icardi@gmail.com>
2016-02-22 13:12:53 +0100

    Added directive

--- demo/demo.js
+++ demo/demo.js
@@ -3,16 +3,21 @@ require("../node_modules/prosemirror/dist/inputrules/autoinput");
 require("../node_modules/prosemirror/dist/menu/tooltipmenu");
 require("../node_modules/prosemirror/dist/menu/menubar");
 
-var pm = window.pm = new ProseMirror({
-  place: document.querySelector(".full"),
-  autoInput: true,
-  tooltipMenu: {selectedBlockMenu: true},
-  menuBar: {float: true},
-  doc: document.querySelector("#content"),
-  docFormat: "dom"
-});
+var angular = require('angular');
 
-document.querySelector("#mark").addEventListener("mousedown", e => {
-  pm.markRange(pm.selection.from, pm.selection.to, {className: "marked"});
-  e.preventDefault();
+angular.module('demo', [])
+.directive('angularProsemirror', function() {
+  return {
+    restrict: "E",
+    link: function(scope, element, attrs){
+      var pm = window.pm = new ProseMirror({
+        place: element[0],
+        autoInput: true,
+        tooltipMenu: {selectedBlockMenu: true},
+        menuBar: {float: true},
+        doc: "Some html content!",
+        docFormat: "html"
+      });
+    }
+  };
 });
